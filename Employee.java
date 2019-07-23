0495f4cbbba66214fee4e580f9a8c30ca15404c6
Petrol95 <petrol195@yandex.ru>
2019-07-23 23:57:49 +0300

    optimized reading file with scanner, added reading file with nio library

--- Employee.java
+++ Employee.java
@@ -1,12 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Java. Level 1. Lesson 5. Homework 5
  *
  * @author Olga Petrova
- * @version dated Sep 25, 2018
+ * @version dated Jul 23, 2019
  */
-
-import java.util.*;
-import java.io.*;
 
 public class Employee {
     // Создать класс "Сотрудник" с полями: фио, должность, email, телефон, зарплата, возраст;
@@ -17,17 +20,7 @@ public class Employee {
     private int salary;
     private int age;
 
-    // Конструктор по умолчанию
-    public Employee() {
-        this.name  = "Unknown";
-        this.position = "";
-        this.email = "";
-        this.telephone = "";
-        this.salary = 0;
-        this.age = 0;
-    }
-
-    // Конструктор с параметрами
+    // Конструктор
     public Employee(String name, String position, String email, String telephone, int salary, int age) {
         this.name  = name;
         this.position = position;
@@ -37,74 +30,72 @@ public class Employee {
         this.age = age;
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public String getPosition(){
-        return position;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getTelephone(){
-        return telephone;
-    }
-
-    public int getSalary(){
-        return salary;
-    }
-
     public int getAge(){
         return age;
     }    
 
     public String toString() {
-        return "Name: " + getName() + ", position: " + getPosition() + ", email: " + getEmail() + ", telephone: " + getTelephone() + ", salary: " + getSalary() + ", age: " + getAge();
+        return  "Name: " + name + ", position: " + position + 
+                ", email: " + email + ", telephone: " + telephone + 
+                ", salary: " + salary + ", age: " + age;
     }
 
     // Чтение списка сотрудников из файла
-    public int readFromFile(Employee[] arr, String fileName) {
-        int i = 0, salary = 0, age = 0;
-        try (Scanner sc = new Scanner(new File(fileName))) {
-            while (sc.hasNextLine() && i < arr.length) {
-                String[] par = sc.nextLine().split(", "); // split string
-                try {
-                    salary = Integer.parseInt(par[4]); // format salary
-                } catch (NumberFormatException e) {
-                    System.err.println("Wrong salary format!");
-                }
-                try {
-                    age = Integer.parseInt(par[5]); // format age
-                } catch (NumberFormatException e) {
-                    System.err.println("Wrong age format!");
-                }
-                arr[i] = new Employee(par[0], par[1], par[2], par[3], salary, age);
-                i++;
+    // вариант 1
+    public static void readScanner(String fileName) {
+        int ind = 0;
+        String str = "";
+        try(Scanner sc = new Scanner(new File(fileName))){
+            while(sc.hasNextLine()) {
+                str += sc.nextLine() + "\n";
+                ind++;
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return i; // number of read lines
+        Employee[] arr = new Employee[ind];
+        String[] lines = str.split("\n");
+        int i = 0;
+        for (String line : lines) {
+            String[] par = line.split(", ");
+            arr[i] = new Employee(par[0], par[1], par[2], par[3],
+                Integer.parseInt(par[4]), Integer.parseInt(par[5]));
+            i++;
+        }
+        getEmp(arr, 40);
+    }
+    
+    // вариант 2
+    public static void readFiles(String fileName) {
+        List<String> arrList = new ArrayList<>();
+        try {
+            arrList = Files.readAllLines(Paths.get(fileName),
+                StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        int i = 0;
+        Employee[] arr = new Employee[arrList.size()];
+        for (String str : arrList) {
+            String[] par = str.split(", ");
+            arr[i] = new Employee(par[0], par[1], par[2], par[3],
+                Integer.parseInt(par[4]), Integer.parseInt(par[5]));
+            i++;
+        }
+        getEmp(arr, 40);
+    }
+    
+    // С помощью цикла вывести информацию только о сотрудниках старше 40 лет
+    public static void getEmp(Employee[] arr, int age) {
+        System.out.println("Employees older than 40:");
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i].getAge() > age)
+                System.out.println(arr[i]);
     }
     
     public static void main(String[] args) {
-        int num = 0; // number of lines, successfully read from file
-        // Создать массив из 5 сотрудников
-        Employee[] empArray = new Employee[5];
-        // Чтение из файла
-        num = new Employee().readFromFile(empArray, "emp.txt");
-        // С помощью цикла вывести информацию только о сотрудниках старше 40 лет
-        System.out.println("Employees older than 40:");
-        for (int i = 0; i < num; i++)
-            if (empArray[i].getAge() > 40)
-                System.out.println(empArray[i].toString());
+        // Чтение из файла, печать сотрудников старше 40 лет
+        // readScanner("emp.txt");
+        readFiles("emp.txt");
     }
 }
- 
-
-
-
- 
